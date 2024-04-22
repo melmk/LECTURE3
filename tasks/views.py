2af0fbe3dcff3997cd2c4cc00f3376bc414ce216
MMK <mmkcodes@gmail.com>
2024-04-22 16:09:41 +1000

    fixed render of index page on tasks app

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,6 @@ tasks =["foo", "bar", "baz"]
 
 # Create your views here.
 def index(request):
-    return render(request, tasks/index.html, {
+    return render(request, "tasks/index.html", {
         "tasks": tasks
     })
