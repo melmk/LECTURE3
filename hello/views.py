9fbd37cdb368c9da2dc2dfbd94773a7a72278e05
MMK <mmkcodes@gmail.com>
2024-04-22 15:07:38 +1000

    added route template for hello app

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html") # Renders in a whole html page instead of just text
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
